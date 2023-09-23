e39fab2de382ef277ec0606333b987054ecc6626
Zhengxu Xia <zxxia@uchicago.edu>
2023-09-22 23:36:12 -0500

    Minor refactoring

--- src/hook/hooks.cc
+++ src/hook/hooks.cc
@@ -1,20 +1,3 @@
-/*
-Copyright (c) 2022 Futurewei Technologies.
-Author: Hao Xu (@hxhp)
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 #include <iomanip>
 #include "hooks.h"
 #include "nvml.h"
@@ -96,7 +79,7 @@ CUresult cuMipmappedArrayCreate_hook(
 }
 
 /**
- * Wenqing: Logic executed before intercepted cuLaunchKernel CUDA call.
+ * Logic executed before intercepted cuLaunchKernel CUDA call.
 */
 CUresult cuLaunchKernel_hook(
 		CUfunction f, uint32_t gridDimX, uint32_t gridDimY,
@@ -105,21 +88,12 @@ CUresult cuLaunchKernel_hook(
 		void** kernelParams, void** extra)
 {
     CUresult cures = CUDA_SUCCESS;
-	uint32_t cost = gridDimX * gridDimY * gridDimZ * blockDimX * blockDimY *
-		blockDimZ;
-    // kernel_launch_time++;
-
-
-    if(kernel_launch_time == 1) {
-        cudaEventCreate(&cu_dummy);
-    }
-
     return cures;
 }
 
 /**
- * Wenqing: Logic executed after cuLaunchKernel CUDA call.
-*/
+ * Logic executed after cuLaunchKernel CUDA call.
+ */
 CUresult cuLaunchKernel_posthook(
 		CUfunction f, uint32_t gridDimX, uint32_t gridDimY,
 		uint32_t gridDimZ, uint32_t blockDimX, uint32_t blockDimY,
